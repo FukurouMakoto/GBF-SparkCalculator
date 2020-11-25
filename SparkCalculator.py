e1beccaa2157a9a2d4bd9f92bad6799da00a7063
Fukurou Makoto <FukurouMakoto@protonmail.com>
2020-11-25 13:10:14 -0500

    changed to account for negative numbers inputted by user

--- SparkCalculator.py
+++ SparkCalculator.py
@@ -11,18 +11,30 @@ def how_many_rolls(total):
 	elif total < 300:
 		untilCanSpark = 300 - int(total)
 		return f"Sorry, you need {str(int(untilCanSpark))} draws until you can spark a character..."	
-def sparkCalculator(total): 
+
+def sparkCalculator(crystals, tix, tenrolls): 
 	return f"You can make a total of {str(int(totalDraws(crystals, tix, tenrolls)))} draws."
 
-print('Welcome to Spark Calculator')
-crystals = input('How many crystals do you have? ')
-tix = input('How many single roll tickets do you have? ')
-tenrolls = input('Finally, how many 10 roll tickets do you have? ')
-total = totalDraws(crystals, tix, tenrolls)
-print(sparkCalculator(total))
-print(how_many_rolls(total))
+def test_for_negative(num):
+	if num < 0:
+		return True
+	else: 
+		return False
+'''
+def main():
+	print('Welcome to Spark Calculator')
+	crystals = input('How many crystals do you have? ')
+	tix = input('How many single roll tickets do you have? ')
+	tenrolls = input('Finally, how many 10 roll tickets do you have? ')
+	for item in [crystals, tix, tenrolls]:
+		if test_for_negative(int(item)):
+			print("Invalid input. Please don't use negative numbers.")
+			main()
+	total = totalDraws(crystals, tix, tenrolls)
+	print(sparkCalculator(crystals, tix, tenrolls))
+	print(how_many_rolls(total))
 
 
-
-
+main()
+'''
 
